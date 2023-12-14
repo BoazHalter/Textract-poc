9f9c0f34b3f5b729b96f82db3b7e377bb96605a3
Boaz Halter <30419068+BoazHalter@users.noreply.github.com>
2023-12-15 00:35:13 +0200

    Update loadFile.py

--- loadFile.py
+++ loadFile.py
@@ -5,6 +5,11 @@ def open_file_dialog():
     file_path = filedialog.askopenfilename()
     if file_path:
         label_file_path.config(text="Selected file: " + file_path)
+        button_process.pack()  # Display the process button after file selection
+
+def process_file():
+    # Replace this function with the actual processing logic for the selected file
+    pass
 
 # Create the main window
 root = tk.Tk()
@@ -22,6 +27,10 @@ label_file_path.pack()
 # Create a button to open the file dialog
 button_browse = tk.Button(root, text="Browse", command=open_file_dialog)
 button_browse.pack()
+
+# Create a button to process the selected file (initially hidden)
+button_process = tk.Button(root, text="Process File", command=process_file)
+button_process.pack_forget()  # Hide the button initially
 
 # Run the main loop
 root.mainloop()
