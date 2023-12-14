3a45818d254871cd25459dbe49b2cb7cbdf47827
Boaz Halter <30419068+BoazHalter@users.noreply.github.com>
2023-12-15 00:19:52 +0200

    Create loadFile.py

---
+++ loadFile.py
@@ -0,0 +1,22 @@
+import tkinter as tk
+from tkinter import filedialog
+
+def open_file_dialog():
+    file_path = filedialog.askopenfilename()
+    if file_path:
+        label_file_path.config(text="Selected file: " + file_path)
+
+# Create the main window
+root = tk.Tk()
+root.title("File Upload")
+
+# Create a label to display the selected file path
+label_file_path = tk.Label(root, text="No file selected")
+label_file_path.pack()
+
+# Create a button to open the file dialog
+button_browse = tk.Button(root, text="Browse", command=open_file_dialog)
+button_browse.pack()
+
+# Run the main loop
+root.mainloop()
