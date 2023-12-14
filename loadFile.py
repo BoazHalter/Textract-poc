fef090bcc957318deb00758cee9316319e45fc7a
Boaz Halter <30419068+BoazHalter@users.noreply.github.com>
2023-12-15 01:34:39 +0200

    Update loadFile.py

--- loadFile.py
+++ loadFile.py
@@ -1,15 +1,32 @@
 import tkinter as tk
 from tkinter import filedialog
+import boto3
+from tkinter import messagebox
 
 def open_file_dialog():
+    global file_path
     file_path = filedialog.askopenfilename()
     if file_path:
         label_file_path.config(text="Selected file: " + file_path)
-        button_process.pack()  # Display the process button after file selection
+        button_process.pack(side=tk.BOTTOM,expand=1)  # Display the process button after file selection
 
 def process_file():
-    # Replace this function with the actual processing logic for the selected file
-    pass
+    if file_path:
+        response = upload_to_s3(file_path)
+        show_response(response)
+
+def upload_to_s3(file):
+    bucket_name = 'your_bucket_name'  # Replace with your S3 bucket name
+    s3 = boto3.client('s3')
+    try:
+        file_name = file.split('/')[-1]  # Extract file name from the path
+        s3.upload_file(file, bucket_name, file_name)
+        return f"{file_name} uploaded successfully to {bucket_name}"
+    except Exception as e:
+        return f"Error uploading file: {e}"
+
+def show_response(response):
+    messagebox.showinfo("S3 Upload Response", response)
 
 # Create the main window
 root = tk.Tk()
@@ -26,10 +43,10 @@ label_file_path.pack()
 
 # Create a button to open the file dialog
 button_browse = tk.Button(root, text="Browse", command=open_file_dialog)
-button_browse.pack()
+button_browse.pack(side=tk.BOTTOM,expand=1)
 
 # Create a button to process the selected file (initially hidden)
-button_process = tk.Button(root, text="Process File", command=process_file)
+button_process = tk.Button(root, text="Upload to AWS S3", command=process_file)
 button_process.pack_forget()  # Hide the button initially
 
 # Run the main loop
